5c0168242eb5d186ff60b7234eb48c556a9b6b9f
Steph Karlsen <becomingsteph@gmail.com>
2025-01-06 00:02:30 -0500

    Contact Form styling

--- lashleads/src/components/ContactForm.js
+++ lashleads/src/components/ContactForm.js
@@ -7,26 +7,35 @@ const ContactForm = () => {
             sx={{
                 display: 'inline-flex',
                 displayDirection: 'row',
-                padding: '75px',
+                padding: '70px',
             }}>
             <FormControl className='--form-control--card'>
-                <Typography> Contact Emily</Typography>
-                <TextField
-                    label="Full Name" required sx={{ padding: '.15rem'}} />
-                <TextField
-                    label='Email' required sx={{ padding: '.15rem'}} />
-                <TextField
-                    label='Subject' sx={{ padding: '.15rem'}} />
-                <TextField
-                    label='Message' sx={{ padding: '.15rem'}} />
-                <Button variant="contained"
+                <Typography
                     sx={{
-                        backgroundColor: '#b76e79',
-                        borderRadius: '50px'
-                    }}>Send Message</Button>
-                <Button sx={{ color: 'black' }}>
-                    Clear Form
-                </Button>
+                        fontSize: '30px',
+                        fontWeight: 'bold'
+                    }}>Contact Emily</Typography>
+                <TextField
+                    label="Full Name" required sx={{ padding: '.15rem' }} />
+                <TextField
+                    label='Email' required sx={{ padding: '.15rem' }} />
+                <TextField
+                    label='Subject' sx={{ padding: '.15rem' }} />
+                <TextField
+                    label='Message' sx={{ padding: '.15rem' }} />
+                <Box>
+                    <Button variant="contained"
+                        sx={{
+                            color: 'black',
+                            backgroundColor: '#CCCCCC',
+                            borderRadius: '50px'
+                        }}>Clear Form</Button>
+                    <Button variant="contained"
+                        sx={{
+                            backgroundColor: '#b76e79',
+                            borderRadius: '50px'
+                        }}>Send Message</Button>
+                </Box>
             </FormControl>
         </Box>
     )
