df18c43b7d044e29ee22c52a0bc14b12893989c8
Steph Karlsen <becomingsteph@gmail.com>
2025-01-20 13:11:14 -0500

    Update home to app.css

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,14 +5,13 @@ import { Box, Grid2 } from "@mui/material";
 
 function HomePage() {
     return (
-        <Box container
-            sx={{
-                backgroundImage: `url(${cover})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-            }}>
-            <ContactForm />
-        </Box>
+        <Grid2
+         className='content'
+         container
+         spacing={2}>
+         <Grid2 item xs={12} sm={4} size={4}>This is the left</Grid2>
+         <Grid2 item xs={12} sm={4} size={4} sx={{ justifyContent: 'flex-end' }}><ContactForm /></Grid2>
+      </Grid2>
     )
 }
 
