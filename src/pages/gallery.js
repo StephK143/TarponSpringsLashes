cfd05fb7b9a034af638d86b53ea509ab1d75c95e
Steph Karlsen <becomingsteph@gmail.com>
2025-01-20 12:56:02 -0500

    Update gallery to app.css

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,9 +1,23 @@
-import { Box } from "@mui/material";
+import { Grid2 } from "@mui/material";
 import React from "react";
+import ContactForm from "../components/ContactForm";
+import cover from '../assets/floral-background.png';
 
 function galleryPage() {
+
    return (
-    <Box>This is the Gallery Page</Box>
+      <Grid2
+         className='content'
+         container
+         spacing={2}
+         sx={{
+            backgroundImage: `url(${cover})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+         }}>
+         <Grid2 item xs={12} sm={4} size={4}>This is the left</Grid2>
+         <Grid2 item xs={12} sm={4} size={4} sx={{ justifyContent: 'flex-end' }}><ContactForm /></Grid2>
+      </Grid2>
    )
 }
 
