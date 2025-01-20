57d21053b413105de50f3d76583f01485764bb3f
Steph Karlsen <becomingsteph@gmail.com>
2025-01-19 22:21:04 -0500

    About Page and data.js created

---
+++ lashleads/src/components/data.js
@@ -0,0 +1,30 @@
+export const AboutData = 
+    {
+        shortDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        longDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+
+export const LocationData = 
+    {
+        locationDescription: "Within 20 mile of Tarpon Springs, FL.",
+        locationMap: "Find a map API for this"
+    }
+
+export const testimonials =
+    [
+        {
+            clientName: "Client 1",
+            clientImage: "",
+            clientReview: "Emily Rocks"
+        },
+        {
+            clientName: "Client 2",
+            clientImage: "",
+            clientReview: "Super! Super!"
+        },
+        {
+            clientName: "Client 3",
+            clientImage: "",
+            clientReview: "Would recommend, 100%"
+        }
+    ]
--- lashleads/src/pages/about.js
+++ lashleads/src/pages/about.js
@@ -1,9 +1,40 @@
-import { Box } from "@mui/material";
+import { List, ListItem, Grid2, Typography } from "@mui/material";
 import React from "react";
+import { AboutData, LocationData } from '../components/data';
+import ContactForm from "../components/ContactForm";
+import cover from '../assets/floral-background.png';
 
-function aboutPage() {
+const aboutPage = () => {
+  
    return (
-    <Box>This is the About Page</Box>
+      <Grid2
+         container
+         sx={{
+            minHeight: '80vh',
+            backgroundImage: `url(${cover})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            flexDirection: 'row',
+            display: 'flex',
+         }}>
+         <List sx={{ flexDirection: 'column', width: '50%', justifyContent: 'flex-start'}}>
+            <ListItem>
+            <Typography>{AboutData.shortDescription}</Typography>
+            </ListItem>
+            <ListItem>
+            <Typography>{AboutData.longDescription}</Typography>
+            </ListItem>
+            <ListItem>
+            <Typography>{LocationData.locationDescription}</Typography>
+            </ListItem>
+            <ListItem>
+            <Typography> {LocationData.locationMap}</Typography>
+            </ListItem>
+         </List>
+         <List sx={{ flexDirection: 'column', justifyContent: 'flex-end', width: '50%' }}>
+            <ContactForm />
+         </List>
+      </Grid2>
    )
 }
 
